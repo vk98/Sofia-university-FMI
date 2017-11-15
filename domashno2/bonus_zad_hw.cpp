25647f20d4b318ac7a3cecfb1c09da7793b87db5
Viktor Kolev <vikmkltd@gmail.com>
2017-11-15 13:08:37 +0200

    Domashno2 - bonus_hw

---
+++ domashno2/bonus_zad_hw.cpp
@@ -0,0 +1,32 @@
+//Viktor Kolev f.n.71776
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a[10],x;
+    cout<<"vuvedete 10 elementa na masiva:";
+    for(int i=0;i<10;i++){
+        cin>>a[i];
+    }
+    cout<<"vuvedete x:";
+    cin>>x;
+    int pointer;
+    for(int i=0;i<10;i++){
+        if(x==a[i]){
+            pointer=i;
+            break;
+        }
+    }
+    int tmp;
+    for(int i=pointer;i<9;i++){
+        a[i]=a[i+1];
+    }
+    a[9]=-1;
+    for(int i=0;i<10;i++){
+        cout<<a[i]<<" ";
+    }
+
+
+    return 0;
+}
